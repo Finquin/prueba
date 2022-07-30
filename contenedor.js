87e1023ad50f3c1764b1f22fbccaffd2250d8b62
Finquin <joe@example.com>
2022-07-30 16:26:20 -0300

    arreglar random

---
+++ contenedor.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+class Contenedor {
+    constructor(ruta) {
+        this.ruta = ruta
+    }
+
+
+    async getAll() {
+        try {
+            const dataArch = await fs.promises.readFile(this.ruta, 'utf8')
+            const dataArchParse = JSON.parse(dataArch)
+            
+            if (dataArchParse.length) {
+                console.log(dataArchParse)
+                   return dataArchParse
+            } else {
+                console.log('no hay productos')               
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async getRandom(){
+        try{
+            const dataFile = await fs.promises.readFile(this.ruta, 'utf-8')
+            const dataFileParse = JSON.parse(dataFile)
+            const max = dataFileParse.length
+            const min = 0
+            const id = Math.ceil(Math.random()* (max - min))
+            const producto = dataFileParse.find(producto => producto.id === id)
+            console.log(dataFileParse , id, producto)
+            if(producto){
+                console.log(producto)
+                return producto
+            }else {
+                console.log("sin resultados")
+            }
+        }catch (error){
+            console.log(error)
+        }
+    }
+}
+
+module.exports = Contenedor
+
+
+
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const Productos = require('./contenedor')
+
+const productos = new Productos('./productos.txt')
+
+
+const app = express()
+const PORT =   8080
+const server = app.listen(PORT, () => {
+    console.log(`Puerto ${server.address().port}`)
+})
+
+
+app.get('/productos', async(req, res) => {
+    const stockProductos = await productos.getAll()
+    res.send(stockProductos)
+   
+})
+
+
+app.get('/productosRandom',async (req, res) => {
+
+    const produdotosRandom = await productos.getRandom()
+    res.send(produdotosRandom)
+
+})
+server.on('error', err => console.log(err))
